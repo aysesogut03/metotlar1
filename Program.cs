cd20748cab468a00167be1c5db860feab7f9a6c6
aysesogut03 <aysesogut2003@gmail.com>
2024-12-10 10:03:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int kareDeger = kareAl(sayi);
+            if (kareDeger > 25)
+            {
+                Console.WriteLine("Sayının karesi 25 den büyüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Sayının karesi 25 den küçüktür.");
+            }
+
+            Console.ReadLine();
+        }
+        
+
+
+        static int kareAl(int a)
+        {
+            return a * a;
+        }
+        
+    }
+}
